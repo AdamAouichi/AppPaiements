4546639bb2945214d77413912a2cd5bbe8fa0a27
MohamedAdam <300140723.monboreal.ca>
2024-10-22 13:52:18 -0400

    new class

---
+++ AppPaiements/AppPaiements/CarteCredit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte {  get; set; }
+        public CarteCredit(double montant, string description, int numeroCarte):base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le montant du paement est:{Montant},la description:{Description},le nnumero de carte est:{NumeroCarte}");
+        }
+    }
+}
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit a = new CarteCredit(100000,"bmw",300140222);
+        CarteCredit b = new CarteCredit(1000000000,"achat maison",300110100);
+        CarteCredit c = new CarteCredit(2000, "achat ordinateur", 300200251);
+
+
+        a.AfficherDetails();
+        b.AfficherDetails();
+        c.AfficherDetails();
+
+
     }
+
 }
